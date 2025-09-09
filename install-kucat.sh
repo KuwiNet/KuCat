83f7dbb4c52adfe32181c23cb5da4d300ec5f087
KuwiNet <darnycn@gmail.com>
2025-09-09 16:20:38 +0800

    Update install-kucat.sh

--- install-kucat.sh
+++ install-kucat.sh
@@ -76,7 +76,7 @@ if grep -qF "$CRON_MARK" /etc/crontabs/root; then
     echo "---- 定时安装任务已存在，跳过 ----"
 else
     echo "---- 写入每天 03:40 定时安装任务 ----"
-    cat >> /etc/crontabs/root <<'EOF'
+    cat >> /etc/crontabs/root <<EOF
 $CRON_MARK
 40 3 * * * export REPO_URL=https://gitee.com/kuwinet/KuCat.git && /usr/bin/install_kucat.sh >/dev/null 2>&1
 EOF
